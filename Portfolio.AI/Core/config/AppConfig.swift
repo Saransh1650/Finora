976e2d70a6b0ab07dae76a9d197b99abd8ed3428
Saransh Singhal <singhalsaransh40@gmail.com>
2025-10-11 19:29:01 +0530

    feat: added xc cloud workflow and env variables

--- Portfolio.AI/Core/config/AppConfig.swift
+++ Portfolio.AI/Core/config/AppConfig.swift
@@ -8,18 +8,29 @@
 import Foundation
 
 class AppConfig {
+
+    private static func secrets() -> [String: Any] {
+        let fileName = "secrets"
+        let path = Bundle.main.path(forResource: fileName, ofType: "json")!
+        let data = try! Data(
+            contentsOf: URL(fileURLWithPath: path),
+            options: .mappedIfSafe
+        )
+        return try! JSONSerialization.jsonObject(with: data) as! [String: Any]
+    }
+
     static var supabaseInitUrl: String {
-        let url = Bundle.main.object(forInfoDictionaryKey: "SUPABASE_INIT_URL")
+        let url =
+            secrets()["SUPABASE_INIT_URL"]
         return "https://\(url as! String)"
     }
 
     static var anonKey: String {
-        return Bundle.main.object(forInfoDictionaryKey: "ANON_KEY") as! String
+        return  secrets()["ANON_KEY"] as! String
     }
 
     static var geminiApiKey: String {
-        return Bundle.main.object(forInfoDictionaryKey: "GEMINI_API_KEY")
-            as! String
+        return  secrets()["GEMINI_API_KEY"] as! String
     }
 
     static var geminiModeFlash: String {
---
+++ Portfolio.AI/ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Stage: PRE-Xcode Build is activated .... "
+
+# Move to the place where the scripts are located.
+# This is important because the position of the subsequently mentioned files depend of this origin.
+cd $CI_PRIMARY_REPOSITORY_PATH/ci_scripts || exit 1
+
+# Write a JSON File containing all the environment variables and secrets.
+printf "{\"SUPABASE_INIT_URL\":\"%s\",\"ANON_KEY\":\"%s\",\"GEMINI_API_KEY\":\"%s\"}" "$SUPABASE_INIT_URL" "$ANON_KEY" "$GEMINI_API_KEY" >> ../secrets.json
+
+echo "Wrote Secrets.json file."
+
+echo "Stage: PRE-Xcode Build is DONE .... "
+
+exit 0
