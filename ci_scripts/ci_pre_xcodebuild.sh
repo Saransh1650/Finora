f1f72a9f3007f78dfb3c4f359163c91e8844626a
Saransh Singhal <singhalsaransh40@gmail.com>
2025-10-11 20:39:30 +0530

    fix: secrets.json path in script

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -7,9 +7,9 @@ echo "Stage: PRE-Xcode Build is activated .... "
 cd $CI_PRIMARY_REPOSITORY_PATH/ci_scripts || exit 1
 
 # Write a JSON File containing all the environment variables and secrets.
-printf "{\"SUPABASE_INIT_URL\":\"%s\",\"ANON_KEY\":\"%s\",\"GEMINI_API_KEY\":\"%s\"}" "$SUPABASE_INIT_URL" "$ANON_KEY" "$GEMINI_API_KEY" >> ../secrets.json
+printf "{\"SUPABASE_INIT_URL\":\"%s\",\"ANON_KEY\":\"%s\",\"GEMINI_API_KEY\":\"%s\"}" "$SUPABASE_INIT_URL" "$ANON_KEY" "$GEMINI_API_KEY" >> ../Portfolio.AI/secrets.json
 
-echo "Wrote Secrets.json file."
+echo "Wrote secrets.json file."
 
 echo "Stage: PRE-Xcode Build is DONE .... "
 
